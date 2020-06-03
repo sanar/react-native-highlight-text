54abd10d9303be2de3d8a4e5a9d2668a99825bc8
Luciano Lima <luciano.lima@sanar.com>
2020-06-03 04:06:17 -0300

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: "@react-native-community",
+  env: {
+    jest: false,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+};
--- index.tsx
+++ index.tsx
@@ -1,39 +1,50 @@
-import React from 'react';
-import { Text as RNText, TextProps, TextStyle } from 'react-native';
-import { findAll, FindAllArgs } from 'highlight-words-core';
+import React from "react";
+import { Text as RNText, TextProps, TextStyle } from "react-native";
+import { findAll, FindAllArgs } from "highlight-words-core";
 
 interface HighlightTextProps extends FindAllArgs, TextProps {
-    highlightStyle?: TextStyle;
-    highlightComponent?: React.ComponentType<TextProps>;
-    textComponent?: React.ComponentType<TextProps>;
-};
+  highlightStyle?: TextStyle;
+  highlightComponent?: React.ComponentType<TextProps>;
+  textComponent?: React.ComponentType<TextProps>;
+}
 
 export default function HighlightText({
+  autoEscape,
+  caseSensitive,
+  sanitize,
+  searchWords,
+  textToHighlight,
+  highlightStyle,
+  highlightComponent,
+  textComponent,
+  ...props
+}: HighlightTextProps) {
+  const chunks = findAll({
     autoEscape,
     caseSensitive,
     sanitize,
     searchWords,
     textToHighlight,
-    highlightStyle,
-    highlightComponent,
-    textComponent,
-    ...props
-}: HighlightTextProps) {
-    const chunks = findAll({autoEscape, caseSensitive, sanitize, searchWords, textToHighlight});
-    const Text = textComponent || RNText;
-    const Highlight = highlightComponent || RNText;
+  });
+  const Text = textComponent || RNText;
+  const Highlight = highlightComponent || RNText;
 
-    return (
-        <Text {...props}>
-            {chunks.map((chunk, index) => {
-                const text = textToHighlight.substr(chunk.start, chunk.end - chunk.start);
+  return (
+    <Text {...props}>
+      {chunks.map((chunk, index) => {
+        const text = textToHighlight.substr(
+          chunk.start,
+          chunk.end - chunk.start
+        );
 
-                return (chunk.highlight) ? (
-                    <Highlight key={`chunk-${index}`} style={highlightStyle}>
-                        {text}
-                    </Highlight>
-                ) : text;
-            })}
-        </Text>
-    );
+        return chunk.highlight ? (
+          <Highlight key={`chunk-${index}`} style={highlightStyle}>
+            {text}
+          </Highlight>
+        ) : (
+          text
+        );
+      })}
+    </Text>
+  );
 }
