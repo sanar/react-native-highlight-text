32a6e424b594ee6697244d017177ec301a1d516f
Luciano Lima <luciano.lima@sanar.com>
2020-06-28 05:11:07 -0300

    chore: update publish settings

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: ['<rootDir>/lib/'],
   testMatch: ['**/?(*.)+(test).js'],
   transformIgnorePatterns: ['node_modules/(?!(jest-)?react-native)'],
 };
